496454b4c0a988e5535b9e9d2705dc211a8be6b7
Tam NTM <tamntm39@gmail.com>
2023-10-12 22:05:53 +0700

    Update Login

--- WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/Controllers/LoginController.cs
@@ -15,6 +15,7 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             var a = 2;
+            var a = 5\;
             var customers = database.Customers.ToList();
             return View(customers);
         }
