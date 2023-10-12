628949fe327d241f6b03ca53a568eb918bf41d5c
Tam NTM <tamntm39@gmail.com>
2023-10-12 22:32:53 +0700

    Update Rename

---
+++ Water_Environment/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Water_Environment.Models;
+
+namespace Water_Environment.Controllers
+{
+    public class LoginController : Controller
+    {
+        Water_EnvironmentEntities database = new Water_EnvironmentEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            //var customers = database.Customers.ToList();
+            return View();
+        }
+    }
+}
---
+++ Water_Environment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Water_Environment.MvcApplication" Language="C#" %>
