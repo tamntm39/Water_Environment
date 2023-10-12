3f1a3d77593af4e1452cc3d144d2738f48860447
Tam NTM <tamntm39@gmail.com>
2023-10-12 21:40:51 +0700

    Init PRJ

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        DangNhapEntities database = new DangNhapEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            var customers = database.Customers.ToList();
+            return View(customers);
+        }
+    }
+}
