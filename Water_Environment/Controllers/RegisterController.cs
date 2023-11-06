a0239c562afa2074fe444a89466fe869a4c69184
Doãn Trung Hoàng <hoangdoan103@gmail.com>
2023-11-06 23:20:16 +0700

    sửa lại form đăng ký

---
+++ Water_Environment/Controllers/RegisterController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using Water_Environment.Models;
+using Water_Environment.Models.Users;
+
+namespace Water_Environment.Controllers
+{
+    public class RegisterController : Controller
+    {
+        Water_EnvironmentEntities _db = new Water_EnvironmentEntities();
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
